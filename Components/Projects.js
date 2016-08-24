89b23f1f0ee9c97b331bfc71b4ad6eb0e5454c69
Sean Marcoux <seanmarcoux@hotmail.com>
2016-08-23 23:23:26 -0400

    Added specific project link to project pictures

--- Components/Projects.js
+++ Components/Projects.js
@@ -34,7 +34,7 @@ var Project = withRouter(React.createClass({
 		return (
 			<table style={{marginLeft:'20%', width:'60%', border: '1px solid orange', borderCollapse:'collapse'}}><tbody>
 				<tr>
-					<td style={{border: '1px solid orange', width:'30%'}}><img src={"../Images/"+this.props.project.imageURL} style={{height:"300px", width:"300px"}}/></td>
+					<td style={{border: '1px solid orange', width:'30%'}}><a onClick={this.goToProjectPage}><img src={"../Images/"+this.props.project.imageURL} style={{height:"300px", width:"300px"}}/></a></td>
 					<td style={{paddingLeft: '20px', border: '1px solid orange'}}>
 						<div style={{color:color}}>{this.props.project.type}</div><br/>
 						Name: <a onClick={this.goToProjectPage}>{this.props.project.name}</a><br/>
